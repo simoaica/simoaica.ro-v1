688673c442414f28e06301e9e5c8b6a8c7ab1ea1
Dan <simoaica@yahoo.com>
2017-03-30 10:38:25 +0300

    adding menus on both sides, smooth scroll to sections, and back to top button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,96 @@
 // Custom JS goes here :)
 
+var photography = document.getElementById('menu-photography');
+var responsive = document.getElementById('menu-responsive');
+var prototype = document.getElementById('menu-prototype');
+var typography = document.getElementById('menu-typography');
+var web = document.getElementById('menu-web');
+var constraints = document.getElementById('menu-constraints');
+var ligature = document.getElementById('menu-ligature');
+var typeset = document.getElementById('menu-typeset');
+var inapoi = document.getElementById('inapoi');
+
+
+photography.addEventListener('click', function(){
+  doScrolling('#photography', 1000);
+});
+responsive.addEventListener('click', function(){
+  doScrolling('#responsive', 1000);
+});
+prototype.addEventListener('click', function(){
+  doScrolling('#prototype', 1000);
+});
+typography.addEventListener('click', function(){
+  doScrolling('#typography', 1000);
+});
+web.addEventListener('click', function(){
+  doScrolling('#web', 1000);
+});
+constraints.addEventListener('click', function(){
+  doScrolling('#constraints', 1000);
+});
+ligature.addEventListener('click', function(){
+  doScrolling('#ligature', 1000);
+});
+typeset.addEventListener('click', function(){
+  doScrolling('#typeset', 1000);
+});
+inapoi.addEventListener('click', function(){
+  doScrolling('#nav-right', 1000);
+});
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("inapoi").className = "vizibil";
+    } else {
+        document.getElementById("inapoi").className = "";
+    }
+}
+// Do scrolling stuff :)
+
+function getElementY(query) {
+  return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top
+}
+
+function doScrolling(element, duration) {
+	var startingY = window.pageYOffset
+  var elementY = getElementY(element)
+  // If element is close to page's bottom then window will scroll only to some position above the element.
+  var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
+	var diff = targetY - startingY
+  // Easing function: easeInOutCubic
+  // From: https://gist.github.com/gre/1650294
+  var easing = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
+  var start
+
+  if (!diff) return
+
+	// Bootstrap our animation - it will get called right before next frame shall be rendered.
+	window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp
+    // Elapsed miliseconds since start of scrolling.
+    var time = timestamp - start
+		// Get percent of completion in range [0, 1].
+    var percent = Math.min(time / duration, 1)
+    // Apply the easing.
+    // It can cause bad-looking slow frames in browser performance tool, so be careful.
+    percent = easing(percent)
+
+    window.scrollTo(0, startingY + diff * percent)
+
+		// Proceed with animation as long as we wanted it to.
+    if (time < duration) {
+      window.requestAnimationFrame(step)
+    }
+  })
+}
+
+
+
+
 // Particles for left side
 
 particlesJS('particule-stanga',
